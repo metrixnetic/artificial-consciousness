d148e4126eb0e68f83758caa7870e3ddaff882a2
richard-undex <inde707x@gmail.com>
2020-08-28 18:09:26 +0300

    int version

--- fer/facial_recog_emotion.r
+++ fer/facial_recog_emotion.r
@@ -1,69 +1,38 @@
 library(jpeg)
+library(image.libfacedetection)
 library(magick)
+library(opencv)
+library(utils)
+library(pracma)
 
 image1 <- image_read("https://3dnews.ru/assets/external/illustrations/2019/04/04/985331/sm.LinusTorvalds_2016-embed.750.jpg")
 
-image1_matrix  <- readJPEG("../Datasets/fer/750.jpg")
-
-detect_faces <- function(x, width, height, step) {
-
-    .Call('_image_libfacedetection_detect_faces', PACKAGE = 'image.libfacedetection', x, width, height, step)
-
-}
-
-image_detect_faces <- function(x) {
-
-  if(inherits(x, "magick-image")) {
-
-    if(!requireNamespace("magick", quietly = TRUE)) {
-
-      stop("image_detect_faces requires the magick package, which you can install from cran with install.packages('magick')")
-
-    }
-
-    meta <- magick::image_info(x)
-
-    if(nrow(meta) != 1) {
-
-      stop("image_detect_faces requires a magick-image containing 1 image only")
-
-    }
-    w <- meta$width
-    h <- meta$height
-    
-    x <- magick::image_data(x, channels = "rgb")
-
-    x <- as.integer(x)
-
-    x <- aperm(x, c(3, 2, 1))
-
-    faces <- detect_faces(x, width = w, height = h, step = 1*w*3)
-
-  } else if(inherits(x, "array")) {
-
-    w <- ncol(x)
-    h <- nrow(x)
-
-    stopifnot(length(dim(x)) == 3 && dim(x)[3] == 3)
-
-    x <- aperm(x, c(3, 2, 1))
-
-    faces <- detect_faces(x, width = w, height = h, step = 1*w*3)
-
-  } else {
-
-    stop("x is not an array nor a magick-image")
-
-  }
-
-  class(faces) <- "libfacedetection"
-
-  return(faces)
-
-}
-
 faces  <- image_detect_faces(image1)
 
-#faces_image1  <- 
+face <- head(faces$detections, 1)
+image_crop(image1, geometry_area(x = face$x, y = face$y, 
+                            width = face$width, height = face$height))
 
-#plot(faces)
+boxcontent <- lapply(seq_len(faces$nr), FUN=function(i){
+
+  face <- faces$detections[i, ]
+
+  image_crop(image1, geometry_area(x = face$x, y = face$y, 
+                              width = face$width, height = face$height))
+})
+
+boxcontent <- do.call(c, boxcontent)
+
+emotion_dict  <- list('Angry' = 0, 'Sad' = 5, 'Neutral' = 4, 'Disgust' = 1, 'Surprise' = 6, 'Fear' = 2, 'Happy' = 3)
+
+#boxcontent  <- image(matrix(boxcontent))
+#face_image  <- ocv_read(boxcontent)
+boxcontent  <- boxcontent %>%
+        image_scale("48x48!") %>%
+        image_quantize(colorspace = 'gray')
+
+boxcontent  <- Reshape(boxcontent[[1]], dim(boxcontent[[1]])[2], dim(boxcontent[[1]])[3])
+#face_image = ocv_resize(face_image, width = 48, height = 48)
+#boxcontent
+print(dim(boxcontent))
+plot(boxcontent)
