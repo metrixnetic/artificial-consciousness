359fd1d6cecbd5f6de9c7acb71c3a2c6227d045e
richard-undex <67050368+richard-undex@users.noreply.github.com>
2021-01-01 00:00:59 +0200

    Update start.R

--- sentiment-analysis/start.R
+++ sentiment-analysis/start.R
@@ -1,46 +1,16 @@
-library(Kmisc)
-
-positive  <-  read.csv('../Datasets/sentiment-analysis/positive.csv', skipNul = TRUE)
-negative  <-  read.csv('../Datasets/sentiment-analysis/negative.csv', skipNul = TRUE)
-
-try_tolower = function(x) {
-  y = NA
-  # tryCatch error
-  try_error = tryCatch(tolower(x), error = function(e) e)
-  # if not an error
-  if (!inherits(try_error, "error"))
-    y = tolower(x)
-  return(y)
-}
-
-clean = function(t) {
-    
-    i  <- 0
-    while (i < length(t)) {
-        
-        i  <- i + 1
-
-        t[i] = gsub('[[:punct:]]', '', t[i])
-        t[i] = gsub('[[:cntrl:]]', '', t[i])
-        t[i] = gsub('\\d+', '', t[i])
-        t[i] = gsub('[[:digit:]]', '', t[i])
-        t[i] = gsub('@\\w+', '', t[i])
-        t[i] = gsub('http\\w+', '', t[i])
-        t[i] = gsub("^\\s+|\\s+$", "", t[i])
-        t[i] = sapply(t[i], function(x)
-            try_tolower(x))
-        t[i] = strsplit(t[i], " ")
-        t[i] = unlist(t[i])
-
-    }
-    return(t)
-               
-}
-
-
-positive  <- as.vector(unlist(positive, use.names=FALSE))
-negative  <- as.vector(unlist(negative, use.names=FALSE))
-positive  <- clean(positive)
-
-print(head(positive))
-
+library(twitteR)
+library(sentiment)
+library(plyr)
+library(ggplot2)
+library(wordcloud)
+library(RColorBrewer)
+setup_twitter_oauth('45NnOSZTY2LzswH5FIXyz7QJn','nScg6Ea3ojWVHJAQf1BE93MnKhq0MT1Eq2rqfDXuw1Bt1CyNaR', access_token=NULL, access_secret=NULL)
+some_tweets = searchTwitter("Trump", n=1500, lang="en")
+some_txt = sapply(some_tweets, function(x) x$getText())
+some_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", some_txt)
+some_txt = gsub("@\\w+", "", some_txt)
+some_txt = gsub("[[:punct:]]", "", some_txt)
+some_txt = gsub("[[:digit:]]", "", some_txt)
+some_txt = gsub("http\\w+", "", some_txt)
+some_txt = gsub("[ \t]{2,}", "", some_txt)
+some_txt = gsub("^\\s+|\\s+$", "", some_txt)
