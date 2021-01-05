8164313897c142b98f1dee6d82b44daf2683d0ad
richard <inde707x@gmail.com>
2021-01-05 21:18:11 +0200

    sor

--- R/cleaning_data.R
+++
@@ -1,5 +0,0 @@
-#Extracting textual part of the tweets
-
-sample=NULL #Initialising
-for (tweet in rahul.tweets)
-sample = c(sample,tweet$getText())
--- R/tweet_extraction.R
+++
@@ -1,5 +0,0 @@
-#Extracting tweets
-rahul.tweets = searchTwitter("@rahul", n=1500)  
-
-#For searching multiple words together
-searchTwitter('Donald+Trump',lang="en",n=N)
