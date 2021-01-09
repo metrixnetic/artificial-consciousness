5f1f5c206b03f7d623fedb5d833f3f291f1270ff
richard-undex <67050368+richard-undex@users.noreply.github.com>
2021-01-09 18:53:57 +0200

    Update start.py

--- fer/start.py
+++ fer/start.py
@@ -45,9 +45,7 @@ while True:
     resized_img = cv2.resize(test_img, (1000, 700))
     cv2.imshow('Facial emotion analysis ',resized_img)
 
-
-
-    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
+    if cv2.waitKey(10) == ord('q'):
         break
 
 cap.release()
