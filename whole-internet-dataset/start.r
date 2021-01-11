2a23eeedc3c53751ec37e004105af248a1d13a52
richard <inde707x@gmail.com>
2021-01-11 20:34:52 +0200

    green version

--- whole-internet-dataset/start.r
+++ whole-internet-dataset/start.r
@@ -16,5 +16,6 @@ library(rjson)
 input  <- "who is vikings"
 url  <- paste0("https://api.duckduckgo.com/?q=", input, "&format=json&pretty=1")
 data  <- fromJSON(file=url)
-print(data)
-
+print(data$AbstractText)
+print(data$RelatedTopics[[1]]$Text)
+print(paste0("if you are really intrested in theme read that: ", data$AbstractURL))
