f852bf36d2c6b1c7f6feb3e51cad198e9d1c9448
richard <inde707x@gmail.com>
2021-01-11 19:51:27 +0200

    on my way

--- whole-internet-dataser/start.r
+++ whole-internet-dataser/start.r
@@ -1,1 +1,20 @@
 # TODO: egdar needed for whole internet dataser creating
+library(tidyverse)
+library(rvest)
+library(stringr)
+library(rebus)
+library(lubridate)
+library(rjson)
+
+#scraping_wiki <- read_html("https://en.wikipedia.org/wiki/Web_scraping")
+
+#cubik  <- scraping_wiki %>%
+ #           html_text()
+#cubik  <- gsub("\\$", "", cubik)
+#cat(cubik)
+
+input  <- "who is vikings"
+url  <- paste0("https://api.duckduckgo.com/?q=", input, "&format=json&pretty=1")
+data  <- fromJSON(file=url)
+print(data)
+
